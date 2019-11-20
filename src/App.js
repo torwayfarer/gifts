d8129858b545ff83c69fdb59ff196f2ab9b2876a
alexander <a.samoyl11@gmail.com>
2019-11-20 22:21:13 +0300

    server deploy

--- src/App.js
+++ src/App.js
@@ -1,60 +1,28 @@
+import React, { Component} from "react";
 
-import React, { Component, Button, Div} from "react";
-import { Container} from 'reactstrap';
-import{ Navbar, Nav, NavDropdown, FormControl, Form } from 'react-bootstrap';
-import axios from "axios";
-import {Spring} from 'react-spring/renderprops';
-import Apply from './Apply';
-import Countdown from './Countdown';
-import PostApply from './PostApply';
+
+import Apply from './components/Apply';
+import PostApply from './components/PostApply';
+
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link,
-  withRouter
+  Route
 } from "react-router-dom";
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      viewCompleted: false,
-      activeItem: {
-        firstName: "",
-        lastName: "",
-        telephone: "",
-        completed: false
-      },
-      formList: []
-    };
-  }
-    componentDidMount() {
-    this.refreshList();
-  }
-  refreshList = () => {
-    axios
-      .get("http://localhost:8000/api/forms/")
-      .then(res => this.setState({ formList: res.data }))
-      .catch(err => console.log(err));
-  };
-  displayCompleted = status => {
-    if (status) {
-      return this.setState({ viewCompleted: true });
-    }
-    return this.setState({ viewCompleted: false });
-  };
 
-    render() {
-    const currentDate = new Date();
-    const year = (currentDate.getMonth() === 11 && currentDate.getDate() > 23) ? currentDate.getFullYear() + 1 : currentDate.getFullYear();
+class App extends Component {
+
+  render() {
     return (
       <Router>
-          <Switch>
-          <Route path='/welcome' component={PostApply}/>
-          <Route path="/" component={Apply}/>
-          </Switch>
+        <Switch>
+
+          <Route exact path='/welcome' component={PostApply}/>
+          <Route exact path="/" component={Apply}/>
+
+        </Switch>
       </Router>
-      )
-    }
+    );
   }
-  export default App;
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';       // add this
 import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
